6ad267393ea54743053ff7060e84ebabcd22d79b
Naveen Kumar Meena <2022pcecrnaveen038@poornima.org>
2025-02-02 21:11:17 +0530

    Update Countdown.py

--- Countdown.py
+++ Countdown.py
@@ -1,11 +1,16 @@
 # Main program
-try:
-    # Get user input
-    number = int(input("Enter a number to start the countdown: "))
-    
-    # Countdown using a while loop
-    while number >= 0:
-        print(number)
-        number -= 1  # Decrease the number by 1
-except ValueError:
-    print("Please enter a valid integer.")
+while True:
+    try:
+        # Get user input
+        number = int(input("Enter a number to start the countdown: "))
+        break  # Exit the loop if input is valid
+    except ValueError:
+        print("Please enter a valid integer.")
+
+# Countdown using a while loop
+print("Countdown starting...")
+while number >= 0:
+    print(number)
+    number -= 1  # Decrease the number by 1
+
+print("Countdown complete!")
