2c1dfddc37e33fafe4023a92dfbafcf56fdb6771
Naveen Kumar Meena <2022pcecrnaveen038@poornima.org>
2025-02-02 21:22:20 +0530

    Create Square_Calculator.py

---
+++ Square_Calculator.py
@@ -0,0 +1,7 @@
+# List of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Looping over the sequence using a for loop
+for number in numbers:
+    square = number ** 2  # Calculate the square of the number
+    print(f"The square of {number} is {square}")
